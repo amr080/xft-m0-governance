62f073053d1d234cc4dea667f1fa7b12f3c2a6f1
Alexander Reed <117864716+amr080@users.noreply.github.com>
2024-08-03 23:10:44 -0400

    Add files via upload

---
+++ _nuxt/C_uqfq_9.js
@@ -0,0 +1,1 @@
+import{a as e,i as a,f as o,ad as n,aa as r,T as t}from"./Tyql0LRw.js";const _=e({__name:"MBadge",props:{version:{default:"success"}},setup(p){return(s,c)=>(a(),o("span",{class:r(s.version)},[n(s.$slots,"default",{},void 0,!0)],2))}}),l=t(_,[["__scopeId","data-v-db3fd6f3"]]);export{l as _};
